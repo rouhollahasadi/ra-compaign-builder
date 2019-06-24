2847428512638447b22272dcbb3ced5898d36307
rouhollahasadi <45329444+rouhollahasadi@users.noreply.github.com>
2019-06-24 13:41:56 +0430

    Create ra-compaign-builder.php

---
+++ ra-compaign-builder.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       RA Campaign Builder
+ * Plugin URI:        https://wordpress.org/plugins/ra-campaign-builder
+ * Description:       Make Campaign and short links for Analytics tools .
+ * Version:           1.0.0
+ * Author:            Rouhollah Asadi
+ * Author URI:        https://wordpress.org
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       ra-campaign-builder
+ * Domain Path:       /languages
+ */
+
